c83c81a57510fd77764c5be257fc60ec15565008
jorgediazok <jorgediazok@gmail.com>
2019-07-26 01:06:25 -0300

    retomando el control

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ var boton_rep = document.getElementById("reproducir");
 function reproducir() {
   if (video.paused) {
     video.play();
-    boton_rep.innerHTML = "<i class=far fa-pause-circle></i>";
+    boton_rep.innerHTML = "<i class='far fa-pause-circle fa-3x'></i>";
   } else {
     video.pause();
+    boton_rep.innerHTML = "<i class='far fa-play-circle fa-3x'></i>";
   }
 }
 
 function reiniciar() {
   video.currentTime = 0;
   video.pause();
-  boton_rep.innerHTML = "<i class=far fa-play-circle></i>";
+  boton_rep.innerHTML = "<i class='far fa-play-circle fa-3x'></i>";
 }
 
 function saltar(value) {
   video.currentTime += value;
 }
+
+//carrousel
